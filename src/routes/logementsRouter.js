19162551f7ec7a40c3e86f22ac6e85f529ba564b
tahaben401 <bentaha2004@gmail.com>
2026-02-14 16:05:15 +0100

    change logement router

--- src/routes/logementsRouter.js
+++ src/routes/logementsRouter.js
@@ -2,13 +2,14 @@
 import { Router } from "express";
 import { getLogements } from "../controllers/LogementsController.js";
 import { getDetailsCompletsChambres } from "../controllers/LogementsController.js";
+import { getChambresParBatiment } from "../controllers/LogementsController.js";
 import protect from "../middleware/protect.js";
 
 const logementrouter = Router();
 
 logementrouter.get("/", protect, getLogements);
 logementrouter.get("/detail_chambre", protect, getDetailsCompletsChambres);
-
+logementrouter.get("/batiment/:batiment", getChambresParBatiment);
 export default logementrouter;
 
 
